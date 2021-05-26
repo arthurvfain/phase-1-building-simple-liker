13962d8e42b1ce37d92e4ca0aeb1932ebd80274d
Arthur V Fain <arthurvfain@gmail.com>
2021-05-26 18:29:41 -0400

    commit tree 052f140c57ad09cfccac84adeade47c5474d532c

--- main.js
+++ main.js
@@ -2,11 +2,70 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.querySelector('#modal').classList.add('hidden')
 
-
-
-
+function startUp()
+{
+  //document.querySelector('#modal').hidden = true;
+  //document.querySelector('#modal').classList.add('hidden')
+  let likes = document.querySelectorAll('.like-glyph')
+  //console.log(likes)
+  //likes.forEach(element => element.addEventListener('click', clickCommand))
+  //likes.forEach(element => element.addEventListener('click',(e) => console.log(e.target.parentElement.parentElement.parentElement.parentElement.id)))
+  //likes.forEach(element => element.addEventListener('click',(e) => clickCommand(e.target.parentElement.parentElement.parentElement.parentElement.id)))
+  likes.forEach(element => element.addEventListener('click',(e) => clickCommand(e.target)))
+}
+function clickCommand(id)
+{
+  let user = id
+  //let likeStatus = document.querySelector('.like-glyph').textContent
+  mimicServerCall().then(changeOfHeart(user)).catch(errorHandle)
+  
+  // //console.log(likeStatus)
+  // if (likeStatus === EMPTY_HEART)
+  // {
+  //   //console.log('omg')
+  //   mimicServerCall().then(changeOfHeart(user)).catch(errorHandle)
+    
+  // }
+  // else if (likeStatus === FULL_HEART)
+  // {
+  //   mimicServerCall().then(resp => console.log(resp)).catch(errorHandle)
+  // }
+}
+function changeOfHeart(idNumber)
+{
+  //console.log('lol', idNumber.textContent)
+  let heart = idNumber.textContent
+  //console.log(heart)
+  if (heart === EMPTY_HEART)
+  {
+    idNumber.textContent = FULL_HEART
+    idNumber.classList.add('activated-heart')
+  }
+  else if (heart ===FULL_HEART)
+  {
+    idNumber.textContent = EMPTY_HEART
+    idNumber.classList.remove('activated-heart')
+  }
+}
+function errorHandle()
+{
+  console.error('error!')
+  showError()
+  setTimeout(hideError, 1000)
+}
+function hideError()
+{
+  //document.querySelector('#modal').hidden = true;
+  document.querySelector('#modal').classList.add('hidden')
+}
+function showError()
+{
+  //document.querySelector('#modal').hidden = false;
+  document.querySelector('#modal').classList.remove('hidden');
+}
+document.addEventListener('DOMContentLoaded', startUp);
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
